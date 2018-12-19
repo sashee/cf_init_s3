ac6280c0ab8551d20518fdc2e4f6e1976fb41bdb
sashee <gsashee@gmail.com>
2018-12-19 13:14:56 +0000

    Use the official cfn-response module

--- s3_file/handler.js
+++ s3_file/handler.js
@@ -1,107 +1,59 @@
 const AWS = require("aws-sdk");
 const s3 = new AWS.S3();
+const response = require("./cfn-response");
 
-const sendResponse = (event, context, responseStatus, Key, id) => {
-	return new Promise((res, rej) => {
-		var responseBody = JSON.stringify({
-			Status: responseStatus,
-			PhysicalResourceId: id,
-			Reason: "See the details in CloudWatch Log Stream: " + context.logStreamName,
-			StackId: event.StackId,
-			RequestId: event.RequestId,
-			LogicalResourceId: event.LogicalResourceId,
-			Data: {Key}
-		});
-	
-		console.log("RESPONSE BODY:\n", responseBody);
-	
-		const https = require("https");
-		const url = require("url");
-	
-		const parsedUrl = url.parse(event.ResponseURL);
-		const options = {
-			hostname: parsedUrl.hostname,
-			port: 443,
-			path: parsedUrl.path,
-			method: "PUT",
-			headers: {
-				"content-type": "",
-				"content-length": responseBody.length
-			}
-		};
-	
-		console.log("SENDING RESPONSE...\n");
-	
-		const request = https.request(options, function(response) {
-			console.log("STATUS: " + response.statusCode);
-			console.log("HEADERS: " + JSON.stringify(response.headers));
-
-			context.done();
-
-			res();
-		});
-	
-		request.on("error", function(error) {
-			console.log("sendResponse Error:" + error);
-
-			context.done();
-
-			rej();
-		});
-	
-		request.write(responseBody);
-		request.end();
-	});
-}
+const replaceParameters = ["Bucket", "KeyPrefix", "KeySuffix"];
 
 const getKey = (keyPrefix, keySuffix, id) => {
 	return `${keyPrefix}-${id}${keySuffix}`;
 }
 
-const getId = (StackId, LogicalResourceId, Bucket, KeyPrefix, KeySuffix) => {
-	return require("crypto").createHash("sha1").update(`${StackId};${LogicalResourceId};${Bucket};${KeyPrefix};${KeySuffix}`).digest("hex").substring(0, 7);
+const getId = (StackId, LogicalResourceId, ...properties) => {
+	return require("crypto").createHash("sha1").update([StackId, LogicalResourceId, ...properties].join(";")).digest("hex").substring(0, 7);
 }
 
-exports.index = async (event, context) => {
-	const {Bucket, KeyPrefix, KeySuffix, Content} = event.ResourceProperties;
+exports.index = (event, context) => {
+	(async () => {
+		const {Bucket, KeyPrefix, KeySuffix, Content} = event.ResourceProperties;
 
-	try {
-		console.log("REQUEST RECEIVED:\n" + JSON.stringify(event));
+		try {
+			console.log("REQUEST RECEIVED:\n" + JSON.stringify(event));
 
-		const {Key, id} = await (async () => {
-			if (event.RequestType === "Delete") {
-				const id = event.PhysicalResourceId;
-				const Key = getKey(KeyPrefix, KeySuffix, id);
+			const {Key, id} = await (async () => {
+				if (event.RequestType === "Delete") {
+					const id = event.PhysicalResourceId;
+					const Key = getKey(KeyPrefix, KeySuffix, id);
 
-				await s3.deleteObject({
-					Bucket,
-					Key
-				}).promise();
+					await s3.deleteObject({
+						Bucket,
+						Key
+					}).promise();
 
-				return {
-					Key,
-					id,
-				};
-			} else {
-				const id = event.RequestType === "Create" || ["Bucket", "KeyPrefix", "KeySuffix"].some((parameter) => event.ResourceProperties[parameter] !== event.OldResourceProperties[parameter]) ? getId(event.StackId, event.LogicalResourceId, Bucket, KeyPrefix, KeySuffix) : event.PhysicalResourceId;
-				const Key = getKey(KeyPrefix, KeySuffix, id);
+					return {
+						Key,
+						id,
+					};
+				} else {
+					const id = event.RequestType === "Create" || replaceParameters.some((parameter) => event.ResourceProperties[parameter] !== event.OldResourceProperties[parameter]) ? getId(event.StackId, event.LogicalResourceId, ...replaceParameters.map((parameter) => event.ResourceProperties[parameter])) : event.PhysicalResourceId;
+					const Key = getKey(KeyPrefix, KeySuffix, id);
 
-				await s3.putObject({
-					Body: Buffer.from(Content, 'binary'),
-					Bucket,
-					Key
-				}).promise();
+					await s3.putObject({
+						Body: Buffer.from(Content, 'binary'),
+						Bucket,
+						Key
+					}).promise();
 
-				return {
-					Key,
-					id,
-				};
-			}
-		})();
+					return {
+						Key,
+						id,
+					};
+				}
+			})();
 
-		await sendResponse(event, context, "SUCCESS", Key, id);
-	}catch(e) {
-		console.error(e);
-		await sendResponse(event, context, "FAILED");
-	}
+			response.send(event, context, response.SUCCESS, {Key}, id);
+		}catch(e) {
+			console.error(e);
+			response.send(event, context, response.FAILED);
+		}
+	})();
 };
